559342672de4cca77af4236a9bd68cf82d8a4afa
sumguytho <sumguytho@gmail.com>
2025-05-08 23:03:37 +0300

    added spider as dependency for spider-cli, it's only runnable through gradle wrapper for now

--- spider-cli/src/main/java/sumguytho/spider/cli/SpiderCli.java
+++ spider-cli/src/main/java/sumguytho/spider/cli/SpiderCli.java
@@ -1,9 +1,13 @@
 package sumguytho.spider.cli;
 
+import sumguytho.spider.Spider;
+
 public class SpiderCli {
 
 	public static void main(String[] args) {
 		System.out.println("spider-cli");
+		Spider sp = new Spider();
+		System.out.println(String.format("spider: %d", sp.getInt()));
 	}
 
 }
---
+++ spider/src/main/java/sumguytho/spider/Spider.java
@@ -0,0 +1,7 @@
+package sumguytho.spider;
+
+public class Spider {
+
+	public int getInt() { return 0; }
+
+}
